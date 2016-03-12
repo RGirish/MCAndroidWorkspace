f35db5efde2d6ba3b84769aca3e319cc1b0e9e6a
Girish Raman <rgirish1994@gmail.com>
2016-03-11 20:55:39 -0700

    Done with WhereAmI - Sender

---
+++ Course Project/SendMyLocation/app/src/main/java/cse535/s16/g32/sendmylocation/LocationUpdateService.java
@@ -0,0 +1,71 @@
+package cse535.s16.g32.sendmylocation;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+
+import com.firebase.client.Firebase;
+
+public class LocationUpdateService extends Service implements LocationListener {
+
+    Firebase ref;
+
+    @Override
+    public void onCreate() {
+        Firebase.setAndroidContext(this);
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 300000, 0, this);
+        ref = new Firebase("https://cse535s16g32.firebaseio.com/");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (connectedToInternet()) {
+            String latitude, longitude;
+            latitude = String.valueOf(location.getLatitude());
+            longitude = String.valueOf(location.getLongitude());
+            ref.child("receivedLocations").child("girish").push().setValue(latitude + "_" + longitude);
+        }
+    }
+
+    private boolean connectedToInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+}
---
+++ Course Project/SendMyLocation/app/src/main/java/cse535/s16/g32/sendmylocation/MainActivity.java
@@ -0,0 +1,15 @@
+package cse535.s16.g32.sendmylocation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startService(new Intent(this, LocationUpdateService.class));
+    }
+}
