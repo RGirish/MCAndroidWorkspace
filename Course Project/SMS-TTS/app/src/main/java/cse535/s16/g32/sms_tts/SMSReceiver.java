71648f854227ac8be74cd60c1b5a57c385610373
Girish Raman <rgirish1994@gmail.com>
2016-03-11 18:19:27 -0700

    Done with SMS TTS

--- Course Project/SMS-TTS/app/src/main/java/cse535/s16/g32/sms_tts/SMSReceiver.java
+++ Course Project/SMS-TTS/app/src/main/java/cse535/s16/g32/sms_tts/SMSReceiver.java
@@ -1,17 +1,62 @@
 package cse535.s16.g32.sms_tts;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
-    public SMSReceiver() {
+
+    public static String getContactName(Context context, String phoneNumber) {
+        ContentResolver contentResolver = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Cursor cursor = contentResolver.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (char c : phoneNumber.toCharArray()) builder.append(c).append(" ");
+        String name = builder.toString().trim();
+
+        if (cursor.moveToFirst()) {
+            name = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+            cursor.close();
+        }
+        return name;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        final Bundle bundle = intent.getExtras();
+        try {
+            if (bundle != null) {
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+                if (pdusObj != null) {
+                    for (Object aPdusObj : pdusObj) {
+                        SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) aPdusObj);
+                        String number = smsMessage.getDisplayOriginatingAddress();
+                        String text = smsMessage.getDisplayMessageBody();
+
+                        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                        audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+
+                        Intent i = new Intent(context, TTSService.class);
+                        i.putExtra("textToSpeak", "Incoming SMS. Message received from " + getContactName(context, number) + ". The message is, " + text);
+                        context.startService(i);
+
+                        Log.e("SMS Received", "Sender - " + getContactName(context, number) + " Message - " + text);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" + e);
+        }
     }
 }
--- Course Project/SMS-TTS/app/src/main/java/cse535/s16/g32/sms_tts/TTSService.java
+++ Course Project/SMS-TTS/app/src/main/java/cse535/s16/g32/sms_tts/TTSService.java
@@ -1,20 +1,45 @@
 package cse535.s16.g32.sms_tts;
 
+import android.annotation.TargetApi;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 
+import java.util.HashMap;
 import java.util.Locale;
 
-public class TTSService extends Service implements TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
+public class TTSService extends Service implements TextToSpeech.OnInitListener {
     private TextToSpeech tts;
     private String textToSpeak;
 
     @Override
     public void onCreate() {
+        textToSpeak = "SMS Received";
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        textToSpeak = intent.getStringExtra("textToSpeak");
         tts = new TextToSpeech(this, this);
-        textToSpeak = "Hello there!";
+        tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onStart(String utteranceId) {
+            }
+
+            @Override
+            public void onDone(String utteranceId) {
+                stopSelf();
+            }
+
+            @Override
+            public void onError(String utteranceId) {
+
+            }
+        });
+        return START_NOT_STICKY;
     }
 
     @Override
@@ -22,14 +47,13 @@ public class TTSService extends Service implements TextToSpeech.OnInitListener, 
         if (status == TextToSpeech.SUCCESS) {
             int result = tts.setLanguage(Locale.US);
             if (result != TextToSpeech.LANG_MISSING_DATA && result != TextToSpeech.LANG_NOT_SUPPORTED) {
-                tts.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ttsGreater21(textToSpeak);
+                } else {
+                    ttsUnder20(textToSpeak);
+                }
             }
         }
-    }
-
-    @Override
-    public void onUtteranceCompleted(String uttId) {
-        stopSelf();
     }
 
     @Override
@@ -44,5 +68,18 @@ public class TTSService extends Service implements TextToSpeech.OnInitListener, 
     @Override
     public IBinder onBind(Intent arg0) {
         return null;
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId = this.hashCode() + "";
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
     }
 }
