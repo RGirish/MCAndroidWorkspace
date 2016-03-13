34331e30b000e57ad1a8e75b7c7c90fe7480f114
Girish Raman <rgirish1994@gmail.com>
2016-03-12 22:09:14 -0700

    Done with NFCRead

--- Course Project/NFCRead/app/src/main/java/girish/raman/nfcread/MainActivity.java
+++ Course Project/NFCRead/app/src/main/java/girish/raman/nfcread/MainActivity.java
@@ -1,43 +1,31 @@
 package girish.raman.nfcread;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
 
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
-
-    static TextToSpeech tts;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tts = new TextToSpeech(this, this);
     }
 
     public void onResume() {
         super.onResume();
         Intent intent = getIntent();
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-            Log.e("ACTION_NDEF_DISCOVERED", "ACTION_NDEF_DISCOVERED");
             processIntent(intent);
         }
     }
 
     void processIntent(Intent intent) {
-        tts = new TextToSpeech(this, this);
         Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
         NdefMessage msg = (NdefMessage) rawMsgs[0];
 
@@ -46,58 +34,11 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         int languageCodeLength = payload[0] & 0063;
         try {
             String stringToSpeak = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-            Log.e("Tag says", stringToSpeak);
-            Toast.makeText(MainActivity.this, stringToSpeak, Toast.LENGTH_SHORT).show();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                ttsGreater21(stringToSpeak);
-            } else {
-                ttsUnder20(stringToSpeak);
-            }
+            Intent intent2 = new Intent(this, TTSService.class);
+            intent2.putExtra("textToSpeak", stringToSpeak);
+            startService(intent2);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS) {
-            int result = tts.setLanguage(Locale.US);
-            tts.setSpeechRate(0.75f);
-            if (tts == null) {
-                Toast.makeText(MainActivity.this, "null", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(MainActivity.this, "not null", Toast.LENGTH_SHORT).show();
-            }
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "This Language is not supported");
-            }
-        } else {
-            Log.e("TTS", "Initialization Failed!");
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (!tts.isSpeaking()) {
-            tts.stop();
-            tts.shutdown();
-            tts = null;
-        }
-    }
-
-    @SuppressWarnings("deprecation")
-    private void ttsUnder20(String text) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
-        finish();
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void ttsGreater21(String text) {
-        String utteranceId = this.hashCode() + "";
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
-        finish();
     }
 }
