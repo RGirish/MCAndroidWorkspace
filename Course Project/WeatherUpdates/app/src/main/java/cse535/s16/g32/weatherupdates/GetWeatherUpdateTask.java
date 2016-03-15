241a258e82e9ddee92da135ec477b2a32f9e7254
Girish Raman <rgirish1994@gmail.com>
2016-03-15 15:37:34 -0700

    Done with weatherupdates

---
+++ Course Project/WeatherUpdates/app/src/main/java/cse535/s16/g32/weatherupdates/GetWeatherUpdateTask.java
@@ -0,0 +1,63 @@
+package cse535.s16.g32.weatherupdates;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetWeatherUpdateTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            String json = streamToString(inputStream);
+            parseJson(json);
+            return "success";
+        } catch (IOException ignored) {
+            return "error";
+        }
+    }
+
+    private void parseJson(String json) {
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONArray weather = root.getJSONArray("weather");
+            String weatherType = ((JSONObject) weather.get(0)).getString("main");
+            Log.e("Weather is ", weatherType);
+        } catch (JSONException e) {
+            Log.e("JSONException", e.toString());
+        }
+    }
+
+    public String streamToString(InputStream inputStream) {
+        final char[] buffer = new char[512];
+        final StringBuilder builder = new StringBuilder();
+        try {
+            Reader reader = new InputStreamReader(inputStream, "UTF-8");
+            for (; ; ) {
+                int bytesRead = reader.read(buffer, 0, buffer.length);
+                if (bytesRead < 0)
+                    break;
+                builder.append(buffer, 0, bytesRead);
+            }
+        } catch (IOException ignored) {
+        }
+        return builder.toString();
+    }
+
+}
--- Course Project/WeatherUpdates/app/src/main/java/cse535/s16/g32/weatherupdates/MainActivity.java
+++ Course Project/WeatherUpdates/app/src/main/java/cse535/s16/g32/weatherupdates/MainActivity.java
@@ -1,7 +1,21 @@
 package cse535.s16.g32.weatherupdates;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //http://api.openweathermap.org/data/2.5/weather?zip=85281,us&APPID=8edc5829a570111458ae29ecf6701e5a
+    }
+
+    public void function(View view) {
+        String city = "seattle";
+        try {
+            Geocoder geocoder;
+            List<Address> addresses;
+            geocoder = new Geocoder(this, Locale.getDefault());
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            List<String> providers = lm.getProviders(true);
+            for (String provider : providers) {
+                Location location = lm.getLastKnownLocation(provider);
+                if (location == null) {
+                    continue;
+                }
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                Address address = addresses.get(0);
+                city = address.getLocality();
+                break;
+            }
+        } catch (IOException e) {
+            Log.e("IOException", e.toString());
+        }
+        new GetWeatherUpdateTask().execute("http://api.openweathermap.org/data/2.5/weather?zip=" + city + ",us&APPID=8edc5829a570111458ae29ecf6701e5a");
     }
 }
