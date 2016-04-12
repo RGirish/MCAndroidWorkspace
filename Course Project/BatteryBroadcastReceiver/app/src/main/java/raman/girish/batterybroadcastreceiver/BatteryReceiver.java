c75a0bdb309737b5c70d4b2b61a9867050d88a68
Girish Raman <rgirish1994@gmail.com>
2016-04-12 12:30:28 -0700

    Done with Battery Receiver

---
+++ Course Project/BatteryBroadcastReceiver/app/src/main/java/raman/girish/batterybroadcastreceiver/BatteryReceiver.java
@@ -0,0 +1,21 @@
+package raman.girish.batterybroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BatteryReceiver extends BroadcastReceiver {
+    public BatteryReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BATTERY_LOW)) {
+            //TTS
+            Log.e("BATTERY_LOW", "BATTERY_LOW");
+        } else if (intent.getAction().equals(Intent.ACTION_BATTERY_OKAY)) {
+
+        }
+    }
+}
---
+++ Course Project/BatteryBroadcastReceiver/app/src/main/java/raman/girish/batterybroadcastreceiver/MainActivity.java
@@ -0,0 +1,13 @@
+package raman.girish.batterybroadcastreceiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
